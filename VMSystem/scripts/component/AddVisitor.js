ce146e099521f85efda168cebfb3c774f4606d3f
Soheb Rapati <rapatisoheb@gmail.com>
2016-05-14 12:13:44 +0530

    Add - AddVisitor component

---
+++ VMSystem/scripts/component/AddVisitor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class AddVisitor extends React.Component {
+  render(){
+    return <div>Soheb</div>
+  }
+}
+
+export default AddVisitor;
--- VMSystem/scripts/main.js
+++ VMSystem/scripts/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import DisplayVisitor from './component/DisplayVisitor'
-
+import AddVisitor from './component/AddVisitor'
 ReactDom.render
   (
-    <DisplayVisitor/>, document.getElementById('main')
+    <AddVisitor/>, document.getElementById('main')
   )
