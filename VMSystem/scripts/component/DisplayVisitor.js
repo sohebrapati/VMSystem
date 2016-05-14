f589f04c9005990a357d8317ac8d2c82f4cca5ad
Sohel Vali <Sohel Vali>
2016-05-14 23:26:32 +0530

    updated displayVisitor form

--- VMSystem/scripts/component/DisplayVisitor.js
+++ VMSystem/scripts/component/DisplayVisitor.js
@@ -36,10 +36,38 @@ class DisplayVisitor extends React.Component{
   }
   render(){
     return(
-      <div>
-            {Object.keys(this.state.visitors).map(this.displayData)}          
-      </div>
+      // <div class="box span4" ontablet="span6" ondesktop="span4">
+      //   <div class="box-content">
+      //     <ul className="dashboard-list">
+      //           {Object.keys(this.state.visitors).map(this.displayData)}
+      //     </ul>
+      //   </div>
+      // </div>
 
+      // comment by Jignesh
+      // <div class="box span4" ontablet="span6" ondesktop="span4">
+			// 		<div class="box-header">
+			// 			<h2><i class="halflings-icon white user"></i><span class="break"></span>Visitors List</h2>
+			// 			<div class="box-icon">
+			// 				<a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+			// 				<a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+			// 			</div>
+			// 		</div>
+			// 		<div class="box-content">
+			// 			<ul class="dashboard-list">
+			// 			      {Object.keys(this.state.visitors).map(this.displayData)}
+			// 			</ul>
+			// 		</div>
+			// 	</div>
+
+      <div>
+        <div className="menu">
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.visitors).map(this.displayData)}
+          </ul>
+        </div>
+
+      </div>
     )
   }
 }
--- VMSystem/scripts/component/SingleVisitor.js
+++ VMSystem/scripts/component/SingleVisitor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import phone from 'react-icons/lib/fa/phone';
 
 class SingleVisitor extends React.Component{
 
@@ -8,19 +9,48 @@ class SingleVisitor extends React.Component{
     var checkingDetails = this.props.visitor.checkinDetails[checkingDetailsCount];
     // var date = (new Date(checkingDetails.inTime)).toDateString();
     // var today = (new Date()).toDateString();
+    var inOut = (checkingDetails.outTime == "" ? "Out" : "In");
     var visitorName = (checkingDetails.noOfVisitors != 0 ? (this.props.visitor.name + '+' + checkingDetails.noOfVisitors) : this.props.visitor.name);
+    var labelImportant = (this.props.visitor.blacklist ? "label label-important" : "label label-success");
+    var isBanned = (this.props.visitor.blacklist ? "Banned" : "");
 
       return(
-        <div>
-            <img src={this.props.visitor.photo} height="50" width="50" />
+        // <div>
+        //     <img src={this.props.visitor.photo} height="50" width="50" />
+        //
+        //        {visitorName} | {this.props.visitor.contactNo}
+        //
+        //        T{checkingDetails.towerNo}-{checkingDetails.flatNo}
+        //             | Reason : {checkingDetails.purpose}
+        //
+        //     <label> {inOut}</label>
+        // </div>
 
-               {visitorName} | {this.props.visitor.contactNo}
+        //comment by Jignesh
+        // <li>
+				// 		{/*<img className="avatar" alt="No Image" src={this.props.visitor.photo} />
+				// 		<strong>{visitorName}</strong> <a href="#">{this.props.visitor.contactNo}</a>
+				// 		T{checkingDetails.towerNo}-{checkingDetails.flatNo}, {checkingDetails.purpose}
+        //     <strong> | Status:</strong> <span className={labelImportant}> {isBanned}</span>
+        //     <label>{inOut}</label>*/}
+        //     <img src={this.props.visitor.photo} height="50" width="50" />
+        //
+        //             {visitorName} | {this.props.visitor.contactNo}
+        //
+        //             T{checkingDetails.towerNo}-{checkingDetails.flatNo}
+        //                  | Reason : {checkingDetails.purpose}
+        //
+        //          <label> {inOut}</label>
+				// </li>
 
-               T{checkingDetails.towerNo}-{checkingDetails.flatNo}
-                    | Reason : {checkingDetails.purpose}
-
-            {/*<input type="Submit" value="In" onChange=""></input>*/}
-        </div>
+        <li className="menu-fish">
+          <img src={this.props.visitor.photo} alt={visitorName} className="imageCurve" />
+          <span>
+             <span className="visitorName">{visitorName}</span> | <phone> {this.props.visitor.contactNo} </phone> | {this.props.visitor.visitorType}
+          </span>
+          <p className="visContent">{checkingDetails.purpose} | T{checkingDetails.towerNo}-{checkingDetails.flatNo}</p>
+          Hello <phone />? 123
+        </li>
       )
   }
 }
